2ba6ee79d9645560a4c037fe1d866ef976b45121
vladmnogotocie <vladorel-93@yandex.ru>
2017-03-22 12:03:34 +0300

    5 5 spring data jpa

--- src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/BaseEntity.java
@@ -1,15 +1,20 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
+
 import javax.persistence.*;
 
 /**
- * User: gkislin
- * Date: 22.08.2014
+ * Do not manipulate new (transient) entries in HashSet/HashMap without overriding hashCode
+ * http://stackoverflow.com/questions/5031614
+ *
+ * @see org.springframework.data.jpa.domain.AbstractPersistable
  */
 @MappedSuperclass
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-public class BaseEntity {
+public class BaseEntity implements Persistable<Integer> {
     public static final int START_SEQ = 100000;
 
     @Id
@@ -30,10 +35,12 @@ public class BaseEntity {
         this.id = id;
     }
 
+    @Override
     public Integer getId() {
         return id;
     }
 
+    @Override
     public boolean isNew() {
         return (getId() == null);
     }
@@ -43,7 +50,7 @@ public class BaseEntity {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
             return false;
         }
         BaseEntity that = (BaseEntity) o;
@@ -55,4 +62,8 @@ public class BaseEntity {
         return (getId() == null) ? 0 : getId();
     }
 
+    @Override
+    public String toString() {
+        return String.format("Entity of type %s with id: %s", getClass().getName(), getId());
+    }
 }
